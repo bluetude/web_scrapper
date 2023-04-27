43f74f569638be311976e54eea11416120733aa3
Marcin Barcz <marcin.barcz@icloud.com>
2023-04-27 20:32:02 +0200

    finished program

--- reverbscraper.py
+++ reverbscraper.py
@@ -9,6 +9,11 @@ mic_list = []
 for mic in li:
     name = mic.find("h4", class_="grid-card__title")
     price = mic.find("span", class_="price-display")
-    mic_list.append((name.text, price.text))
+    link = mic.find("a", class_="grid-card__inner")
+    link = link["href"]
+    link = f"https://reverb.com{link}"
+    img = mic.find("img", loading="lazy")
+    img = img["src"]
+    mic_list.append((name.text, price.text, link, img))
 
 print(mic_list)
